4c16568c943c5279c8aab4fae8dbcf23b4364520
Alexander Schmidt <alexander.informatik@googlemail.com>
2013-05-29 04:29:41 -0700

    user story 32+33 finished .99

--- Root/src/main/java/de/osramos/ss13/proj1/web/TaskdbShowMapController.java
+++ Root/src/main/java/de/osramos/ss13/proj1/web/TaskdbShowMapController.java
@@ -1,12 +1,12 @@
 package de.osramos.ss13.proj1.web;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import de.osramos.ss13.proj1.model.Taskdb;
 
 @RequestMapping("/trainee/taskdb/showmap")
 @Controller
@@ -17,7 +17,26 @@ public class TaskdbShowMapController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public String getMap(@PathVariable Long id, ModelMap model) {
  
-		model.addAttribute("taskid", id);
+    	model.addAttribute("taskid", id);
+    	
+    	try
+    	{
+	    	Taskdb o = (Taskdb) Taskdb.findTaskdbsById(id).getSingleResult();
+	    	if(o != null)
+	    	{
+	    		
+	    		model.addAttribute("taskname", o.getTaskname());
+	    		model.addAttribute("gpsstart", o.getGps_Start());
+	    		model.addAttribute("gpsend", o.getGps_End());
+	    	}
+	    	else
+	    	{
+	    		model.addAttribute("taskid", "taskid not found");
+	    	}
+    	}
+    	catch(Exception e)
+    	{}
+    	
 		return "trainee/taskdb/showmap/index";
  
 	}
