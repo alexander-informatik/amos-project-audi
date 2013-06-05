c13124d9dfb44b4da2a2432590663bc52620972d
Alexander Schmidt <alexander.informatik@googlemail.com>
2013-06-05 13:50:04 -0700

    Sprint 08 / Production data / As a user i want to see production data to get a feel of the application

--- Root/src/main/java/de/osramos/ss13/proj1/security/AuthenticationByUserdb.java
+++ Root/src/main/java/de/osramos/ss13/proj1/security/AuthenticationByUserdb.java
@@ -1,31 +1,5 @@
 package de.osramos.ss13.proj1.security;
 
-/*
- * #%L
- * proj1
- * %%
- * Copyright (C) 2013 Amos-Project
- * %%
- * Copyright (c) 2013 by Alexander Schmidt, Sascha Ströbel, Nina Aures, Riehle, http://dirkriehle.com
- * -
- * This file is part of the Amos Project - Productive Games application.
- * -
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * -
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * -
- * You should have received a copy of the GNU Affero General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
- * #L%
- */
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,9 +19,8 @@ import org.springframework.util.StringUtils;
 
 import de.osramos.ss13.proj1.model.Userdb;
 
-public class AuthenticationByUserdb
-		extends
-			AbstractUserDetailsAuthenticationProvider {
+public class AuthenticationByUserdb extends
+		AbstractUserDetailsAuthenticationProvider {
 
 	@Override
 	protected void additionalAuthenticationChecks(UserDetails userDetails,
@@ -61,9 +34,7 @@ public class AuthenticationByUserdb
 	protected UserDetails retrieveUser(String username,
 			UsernamePasswordAuthenticationToken authentication)
 			throws AuthenticationException {
-		// populate with default usernames and passwords
-		CreateUsers();
-
+		
 		String password = (String) authentication.getCredentials();
 		if (!StringUtils.hasText(password)) {
 			throw new BadCredentialsException("Please enter password");
@@ -98,69 +69,5 @@ public class AuthenticationByUserdb
 				authorities);
 	}
 
-	private void CreateUsers() {
-		Userdb user = null;
-
-		try {
-			user = null;
-			user = Userdb.findUserdbsByUsernameEquals("admin")
-					.getSingleResult();
-		} catch (Exception e) {
-		} finally {
-			try {
-				if (null == user) {
-					user = new Userdb();
-					user.setFirstname("admin");
-					user.setLastname("ADMIN");
-					user.setUsername("admin");
-					user.setUserrole("admin");
-					user.setPassword("admin");
-					user.persist();
-				}
-			} catch (Exception e) {
-			}
-		}
-
-		try {
-			user = null;
-			user = Userdb.findUserdbsByUsernameEquals("senior")
-					.getSingleResult();
-		} catch (Exception e) {
-		} finally {
-			try {
-				if (null == user) {
-					user = new Userdb();
-					user.setFirstname("senior");
-					user.setLastname("SENIOR");
-					user.setUsername("senior");
-					user.setUserrole("senior");
-					user.setPassword("senior");
-					user.persist();
-				}
-			} catch (Exception e) {
-			}
-		}
-
-		try {
-			user = null;
-			user = Userdb.findUserdbsByUsernameEquals("trainee")
-					.getSingleResult();
-		} catch (Exception e) {
-		} finally {
-			try {
-				if (null == user) {
-					user = null;
-					user = new Userdb();
-					user.setFirstname("trainee");
-					user.setLastname("TRAINEE");
-					user.setUsername("trainee");
-					user.setUserrole("trainee");
-					user.setPassword("trainee");
-					user.persist();
-				}
-			} catch (Exception e) {
-			}
-		}
-
-	}
+	
 }
