e3343f3681e96e8d22b4a85169b0d0b6b46f810d
Alexander Schmidt <alexander.informatik@googlemail.com>
2013-06-08 13:37:26 -0700

    user story 54 / Sprint / Alexander Schmidt / Task creation / Permission to record a route / As a senior, I need the opportunity to agree to the recording and storing of my route (button), in order to allow saving my private data.

--- Root/src/main/java/de/osramos/ss13/proj1/web/SeniorRecordRoute.java
+++ Root/src/main/java/de/osramos/ss13/proj1/web/SeniorRecordRoute.java
@@ -28,23 +28,35 @@ package de.osramos.ss13.proj1.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import de.osramos.ss13.proj1.model.Taskdb;
 
 @RequestMapping("/senior/recordroute/**")
 @Controller
 public class SeniorRecordRoute {
 
 	@RequestMapping(method = RequestMethod.POST, value = "{id}")
-	public void post(@PathVariable Long id, ModelMap modelMap,
+	public void post(@PathVariable Boolean recordpermission, ModelMap modelMap,
 			HttpServletRequest request, HttpServletResponse response) {
 	}
+	
+	@RequestMapping(produces = "text/html")
+	public String index(
+			@RequestParam(value = "recordpermission", required = false) Boolean recordpermission,
+			Model uiModel) {
 
-	@RequestMapping
-	public String index() {
-		return "senior/recordroute/index";
+			uiModel.addAttribute("recordpermission", recordpermission);
+		
+			return "senior/recordroute/index";
 	}
+	
 }
